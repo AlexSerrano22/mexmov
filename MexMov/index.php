8004b6d4dbdfa284b604e1a176fcb79dc343e7b5
AlexSerrano22 <serrano_ayn@hotmail.com>
2013-10-23 00:45:10 -0500

    actualizacion 23/10/13

--- MexMov/index.php
+++ MexMov/index.php
@@ -3,6 +3,12 @@
 date_default_timezone_set("America/Mexico_City");
 $fecha= date("Y-M-d",time());
 $date = explode("-", $fecha);
+session_start();
+if(isset($_SESSION['user']) ){
+		$user = $_SESSION['user'];
+	}else{
+		$user = 'anonimo';		
+	}
  ?>
 <!DOCTYPE html>
 <!--[if IE 8]> 				 <html class="no-js lt-ie9" lang="en" > <![endif]-->
@@ -24,6 +30,40 @@ $date = explode("-", $fecha);
 
 		<script src="js/vendor/custom.modernizr.js"></script>
 
+		<!-- INICIO DE LA IMAGENES -->
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:700,300,300italic' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="css/demo.css" />
+        <link rel="stylesheet" type="text/css" href="css/styles.css" />
+		<!--[if lt IE 9]>
+		<link rel="stylesheet" type="text/css" href="css/style_ie.css" />
+		<![endif]-->
+		<!-- jQuery -->
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+		<!-- jmpress plugin -->
+		<script type="text/javascript" src="js/jmpress.min.js"></script>
+		<!-- jmslideshow plugin : extends the jmpress plugin -->
+		<script type="text/javascript" src="js/jquery.jmslideshow.js"></script>
+		<script type="text/javascript" src="js/modernizr.custom.48780.js"></script>
+		<noscript>
+			<style>
+			.step {
+				width: 100%;
+				position: relative;
+			}
+			.step:not(.active) {
+				opacity: 1;
+				filter: alpha(opacity=99);
+				-ms-filter: "progid:DXImageTransform.Microsoft.Alpha(opacity=99)";
+			}
+			.step:not(.active) a.jms-link{
+				opacity: 1;
+				margin-top: 40px;
+			}
+			</style>
+		</noscript>
+
+		<!--fin de codigo en head de las imagenes-->
+
 
 	</head>
 	
@@ -41,7 +81,7 @@ $date = explode("-", $fecha);
 									<header>
 
 										
-										<h2><a href="#">Bienvenido a MexMov</a></h2>
+										<h2>Bienvenido a MexMov</h2>
 										<span class="byline">Un sitio donde podras conocer tus niveles de sedentarismo</span>
 
 									
@@ -55,34 +95,51 @@ $date = explode("-", $fecha);
 		
 										
 									</div>
-									<a href="#" class="image image-full"><img src="images/logomexmov.png" alt="" /></a>
-									
+									<!-- CODIGO DE IMAGENES -->
+									<section id="jms-slideshow" class="jms-slideshow">
+				<div class="step" data-color="color-6">
+					<div class="jms-content">
+						<h3>Eres Sedentario?</h3>
+						<p>realiza el test para conocer tu nivel de sedentarismo ...</p>
+						<a class="jms-link" href="#">Read more</a>
+					</div>
+					<img src="images/logo.png" width="450px"/>
+				</div>
+				<div class="step" data-color="color-2" data-y="500" data-scale="0.4" data-rotate-x="30">
+					<div class="jms-content">
+						<h3>Holy cannoli!</h3>
+						<p>But as the riper should by time decease, his tender heir might bear his memory</p>
+						<a class="jms-link" href="#">Read more</a>
+					</div>
+					<img src="images/2.png"  />
+				</div>
+				<div class="step" data-color="color-3" data-x="2000" data-z="3000" data-rotate="170">
+					<div class="jms-content">
+						<h3>No time to waste</h3>
+						<p>Within thine own bud buriest thy content and, tender churl, makest waste in niggarding</p>
+						<a class="jms-link" href="#">Read more</a>
+					</div>
+					<img src="images/3.png" />
+				</div>
+				<div class="step" data-color="color-4" data-x="3000">
+					<div class="jms-content">
+						<h3>Supercool!</h3>
+						<p>Making a famine where abundance lies, thyself thy foe, to thy sweet self too cruel</p>
+						<a class="jms-link" href="#">Read more</a>
+					</div>
+					<img src="images/4.png" />
+				</div>
+				<div class="step" data-color="color-5" data-x="4500" data-z="1000" data-rotate-y="45">
+					<div class="jms-content">
+						<h3>Did you know that...</h3>
+						<p>Thou that art now the world's fresh ornament and only herald to the gaudy spring</p>
+						<a class="jms-link" href="#">Read more</a>
+					</div>
+					<img src="images/5.png" />
+				</div>
+			</section>
+			<!--FIN DE CODIGO -->
 								</article>
-						
-							
-							<!-- Pager -->
-								<div class="pager">
-									<!--<a href="#" class="button previous">Previous Page</a>-->
-									<div class="pages">
-										<a href="#" class="active">1</a>
-										<a href="#">2</a>
-										<a href="#">3</a>
-										<a href="#">4</a>
-										<span>&hellip;</span>
-										<a href="#">20</a>
-									</div>
-									<a href="#" class="button next">Siguiente</a>
-								</div>
-									<!-- Copyright -->
-							<div id="copyright">
-								<p>
-									&copy; 2013 An Untitled Site.<br />
-									Images: <a href="http://n33.co">n33</a>, <a href="http://fotogrph.com">fotogrph</a>, <a href="http://iconify.it">Iconify.it</a>
-									Design: <a href="http://html5up.net/">HTML5 UP</a>
-								</p>
-							</div>
-
-
 						</div>
 					</div>
 
@@ -92,13 +149,15 @@ $date = explode("-", $fecha);
 						<!-- Logo -->
 							<div id="logo">
 								<a href="#" class="image image-full"><img src="images/logomexmov.png" alt="" /></a>
+								<h4><?php if($user != 'anonimo')echo $user; ?></h4>
+								
 							</div>
 					
 						<!-- Nav -->
 							<nav id="nav">
 								<ul>
 									<li class="current_page_item"><a href="#">Home</a></li>
-									<li><a href="#" data-reveal-id="login">Iniciar Sesion</a></li>
+									<li><?php if($user == 'anonimo'){echo '<a href="#" data-reveal-id="login">Iniciar Sesion';}else{echo '<a href="logout.php">Cerrar Sesion';} ?></a></li>
 									<li><a href="#">Contacto</a></li>
 								</ul>
 							</nav>
@@ -126,8 +185,14 @@ $date = explode("-", $fecha);
 
 			</div>
 
-			 <div id="login" class="reveal-modal" style="display: none;" >
-		    	<form action="veriflog.php" method = "POST">
+			 <div id="login" class="reveal-modal small" style="display: none;" >
+
+			 	<div class="row">
+					      <div class="large-12 columns">
+					        <center><img src="images/logomexmov.png" alt="" /></center>
+					      </div>
+					    </div>
+		    	<form action="login.php" method = "POST">
 				  <fieldset>
 				    <legend>Login</legend>
 					    <div class="row">
@@ -145,8 +210,8 @@ $date = explode("-", $fecha);
 					  	</div>
 
 					    <div class="row">
-					      <div class="large-4 columns">
-					        <input type="submit" value="Iniciar Sesion" class="button next">
+					      <div class="large-6 columns">
+					        <input type="submit" value="Iniciar Sesion" class="button round">
 					      </div>
 					    </div>
 				  </fieldset>
@@ -164,7 +229,23 @@ $date = explode("-", $fecha);
 
 
   </script>
-  
+  <!-- CODIGO IMAGENES -->
+  		<script type="text/javascript">
+			$(function() {
+				
+				var jmpressOpts	= {
+					animation		: { transitionDuration : '1.0s' }
+				};
+				
+				$( '#jms-slideshow' ).jmslideshow( $.extend( true, { jmpressOpts : jmpressOpts }, {
+					autoplay	: true,
+					bgColorSpeed: '0.8s',
+					arrows		: true
+				}));
+				
+			});
+		</script>
+
   <script src="js/foundation.min.js"></script>
   <!--
   
